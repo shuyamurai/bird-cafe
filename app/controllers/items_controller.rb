062c3e9dd7338aa2fdc565461e5592e86ae8db18
shuyamurai <shuyamurai@yahoo.co.jp>
2020-08-27 13:28:38 +0900

    create item function4

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
 
 
