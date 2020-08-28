ceb93dfb8e7db49af76b970e54b022e23d841f72
shuyamurai <shuyamurai@yahoo.co.jp>
2020-08-28 18:45:47 +0900

    create like3

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -21,18 +21,21 @@ class LikesController < ApplicationController
   before_action :set_item
 
   def create
-    @like = Like.create(user_id: current_user.id, item_id: @items.id)
-    @likes = Like.where(like_id: @items.id)
+    @like = Like.create(user_id: current_user.id, item_id: @item.id)
+    @likes = Like.where(like_id: @item.id)
   end
 
   def destroy
-    @like = current_user.likes.find_by(item_id: @items.id)
+    @like = current_user.likes.find_by(item_id: @item.id)
     @like.destroy
-    @likes = Like.where(like_id: @items.id)
+    @likes = Like.where(like_id: @item.id)
   end
 
   private
   def set_item
-    @items = Item.find(params[:items_id])
+    @item = Item.find(params[:items_id])
   end
 end
+
+
+
