3af4c1c03c23a7efbe14db24222d1bd32b2fbbeb
densetsu <tab.23.yude@gmail.com>
2020-09-02 07:42:53 +0900

    コメント機能の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,18 @@
 class CommentsController < ApplicationController
 
   def create
-    "asdf"
-    comment = Comment.create(comment_params)
-    redirect_to "#"
+    comment = Comment.new(comment_params)
+    if comment.save
+      redirect_to "/"
+    else
+      asdf
+    end
   end
 
   private
   def comment_params
     # params.require(:comment).permit(:comment).merge(user_id: current_user.id, item_id: params[:item_id])
-    params.require(:comment).permit(:user_id , :item_id)
+    params.require(:comment).permit(:user_id, :item_id, :comment)
   end
 
 end
