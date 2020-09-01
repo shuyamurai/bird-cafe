b7f49ca93233a78ad75d68d0afca12f5b618c9b0
shuyamurai <shuyamurai@yahoo.co.jp>
2020-09-01 21:00:02 +0900

    create comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,15 @@
 class CommentsController < ApplicationController
 
   def create
+    "asdf"
     comment = Comment.create(comment_params)
-    redirect_to "/items/#{comment.item.id}"
+    redirect_to "#"
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:comment).merge(user_id: current_user.id, item_id: params[:item_id])
+    # params.require(:comment).permit(:comment).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:comment).permit(:user_id , :item_id)
   end
 
 end
