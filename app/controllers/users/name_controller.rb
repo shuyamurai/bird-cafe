0dc915f9ea8050a6b129149ec56e4cc4cbf92dce
shuyamurai <shuyamurai@yahoo.co.jp>
2020-09-11 23:22:16 +0900

    aws

--- app/controllers/users/name_controller.rb
+++ app/controllers/users/name_controller.rb
@@ -1,7 +1,10 @@
-class Users::NamesController < Devise::PasswordsController
+class Users::NamesController 
   before_action :check_guest, only: :create
 
   def check_guest
     redirect_to root_path, alert: 'ゲストユーザーの変更はできません。' if params[:user][:email].downcase == 'guest@example.com'
   end
 end
+
+
+# class Users::NamesController < Devise::PasswordsController
